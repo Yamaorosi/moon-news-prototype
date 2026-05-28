dab5f74c7c51314944d32a9a13d787a0de602248
Yamaorosi <k862160@kansai-u.ac.jp>
2026-05-28 16:12:54 +0900

    fix:直したよ

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/moon-news-prototype/',
-})
   base: '/moon-news-prototype/',
 })
